37e97d2d8536bb5e5703731fee3e7ec43ad0654f
Denilson Arruda da Silva <denilsonarruda@outlook.com>
2023-04-28 06:27:14 -0300

    update

---
+++ src/controllers/CompradorController.js
@@ -0,0 +1,65 @@
+const Comprador = require("../models/comprador.model");
+const bcript = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth");
+
+module.exports = {
+  async signup(
+    nome,
+    email,
+    senha,
+    descricaoDeCompra,
+    endereco,
+    pessoaJuridica,
+    cpf,
+    cnpj
+  ) {
+    try {
+      const findComprador = await Comprador.findOne({ cpf });
+      if (!findComprador) {
+        const newComprador = await Comprador.signup({
+          nome,
+          email,
+          senha: bcript.hashSync(senha, 8),
+          descricaoDeCompra,
+          endereco,
+          pessoaJuridica,
+          cpf,
+          cnpj,
+        });
+        return { message: newComprador, status: 200 };
+      } else {
+        return {
+          message: "Um comprador com esse CPF já está cadastrado",
+          status: 400,
+        };
+      }
+    } catch (error) {
+      return { message: error, status: 400 };
+    }
+  },
+  async signin(cpf, senha) {
+    try {
+      const foundComprador = await Comprador.findOne({ cpf });
+      if (!foundComprador) {
+        return { message: "CPF não encontrado", status: 400 };
+      } else {
+        const passwordMatch = await bcript.compare(senha, foundComprador.senha);
+
+        if (!passwordMatch) {
+          return {
+            message: "Credenciais fornecidas inválidas",
+            status: 400,
+          };
+        } else {
+          const token = jwt.sign({ id: foundComprador._id }, config.secret, {
+            expiresIn: 86400,
+          });
+          return { message: token, status: 200 };
+        }
+      }
+    } catch (error) {
+      return { message: error, status: 400 };
+    }
+  },
+};
---
+++ src/routes/CompradorRoutes.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const CompradorController = require("../controllers/CompradorController");
+const CompradorAuth = require("../middlewares/CompradorAuth");
+const router = express.Router();
+
+// Cadastro do comprador
+router.post("/signup", async (req, res) => {
+  try {
+    const {
+      nome,
+      email,
+      senha,
+      descricaoDeCompra,
+      endereco,
+      pessoaJuridica,
+      cpf,
+      cnpj,
+    } = req.body;
+
+    const response = await CompradorController.signup(
+      nome,
+      email,
+      senha,
+      descricaoDeCompra,
+      endereco,
+      pessoaJuridica,
+      cpf,
+      cnpj
+    );
+    return res
+      .status(response.status)
+      .json({ message: response.message, status: response.status });
+  } catch (error) {
+    return res.status(400).json({ message: error, error: 400 });
+  }
+});
+
+// Login do comprador
+router.post("/signin", async (req, res) => {
+  try {
+    const { cpf, senha } = req.body;
+
+    const result = await CompradorController.signin(cpf, senha);
+    return res
+      .status(result.status)
+      .json({ message: result.message, status: result.status });
+  } catch (error) {
+    return res.status(400).json({ message: error, error: 400 });
+  }
+});
+
+module.exports = router;
